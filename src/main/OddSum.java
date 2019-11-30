931b23bff8eea723e7a6a6211f20af612c9829db
Adrien Souquere <adriensouquere@gmail.com>
2019-11-30 15:35:25 +0100

    réslutat

--- src/main/OddSum.java
+++ src/main/OddSum.java
@@ -17,7 +17,12 @@ public class OddSum {
             System.out.println();
         }
         for (int i = 0; i < args.length; i++) {
-            listInt.add(Integer.parseInt(args[i]));
+        	try {
+            	listInt.add(Integer.parseInt(args[i]));
+			} catch (NumberFormatException e) {
+				return -1;
+			}
+        
         }
 
         res = sum(listInt);
